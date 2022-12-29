bf0dac4a69b9628b0c341f01dc733006a38ad2e3
William <chunyinleung@etnet.com.hk>
2022-12-29 17:36:29 +0800

    sql create

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';
-$db_user = 'id18278535_william107';
+$db_user = 'william107';
 $db_password = '107B@ka4579trpg';
 $db_name = 'id18278535_107trpg';
 ?>
--- config_production.php
+++ config_production.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'https://databases.000webhost.com';
+$host = 'localhost';
 $db_user = 'id18278535_william107';
 $db_password = '107B@ka4579trpg';
 $db_name = 'id18278535_107trpg';
